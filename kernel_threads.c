d37b1b0ad10acc52b4eaae65b4b4db8ec6cc9ea9
giorgos tsi <giorgostsikri@hotmail.com>
2022-11-22 21:24:50 +0200

    Nothing important

--- kernel_threads.c
+++ kernel_threads.c
@@ -21,6 +21,14 @@ void start_thread(){
 
 /** 
   @brief Create a new thread in the current process.
+  Parameters:
+    @param task to be executed
+    @param lenght of arguments
+    @param array of arguments
+
+  Returns:
+    @return the tid(ptcb) of the new thread
+    @return NOTHREAD if task is null
   */
 Tid_t sys_CreateThread(Task task, int argl, void* args)
 {
@@ -61,6 +69,7 @@ Tid_t sys_CreateThread(Task task, int argl, void* args)
   return (Tid_t) new_ptcb;
 }
 
+
 /**
   @brief Return the Tid of the current thread.
  */
@@ -71,28 +80,37 @@ Tid_t sys_ThreadSelf()
 
 /**
   @brief Join the given thread.
+  Parameters:
+    @param the tid(ptcb) of the thread to join
+    @param the address to store the exit value.
   */
 int sys_ThreadJoin(Tid_t tid, int* exitval)
 {
+
+
   TCB* invoker_thread = cur_thread(); // the invoked thread 
   PTCB* invoker_ptcb = invoker_thread->ptcb; // the invoked thread ptcb 
   PTCB* invoked_ptcb= (PTCB*) tid; // The thread to join
+
 
   // if the thread to join to is not in the same process or does not exist
   if(rlist_find(&(CURPROC->ptcb_list), invoked_ptcb, NULL) == NULL){
     //fprintf(stderr, "There is no thread with the given tid in this process.");
     return -1;
   }
+
   // if the invoker thread tried to join itself
   else if(invoker_ptcb == invoked_ptcb){
     //fprintf(stderr, "The given tid corresponds to the current thread.");
     return -1;
   }
+
   // if the thread to join to is detached
   else if(invoked_ptcb->detached == 1){
     //fprintf(stderr, "The tid corresponds to a detached thread.");
     return -1;
   }
+
   // if the thread to join to is exited
   else if(invoked_ptcb->exited == 1){
     //fprintf(stderr, "The tid corresponds to an exited thread.");
@@ -105,11 +123,13 @@ int sys_ThreadJoin(Tid_t tid, int* exitval)
   // update the refcount as a thread waits to join this one
   invoked_ptcb->refcount++;
 
-  // sleep until the other thread exits or becomes detached
-  // use while loop in case there is a false wake up call 
+  /*sleep until the other thread exits or becomes detached
+   use while loop in case there is a false wake up call */ 
   while(invoked_ptcb->detached == 0 && invoked_ptcb->exited == 0){
     kernel_wait(&invoked_ptcb->exit_cv, SCHED_USER); 
   }
+
+
   // the current thread was awoken (this might take some time)
   //decrease the refcount we have one less waiting thread
   invoked_ptcb->refcount--;
@@ -142,10 +162,12 @@ int sys_ThreadDetach(Tid_t tid)
   if(tid == NULL){
     return -1;
   }
+
   // if there is no thread with the given tid in this process
   else if(rlist_find(&(CURPROC->ptcb_list), thread_to_detach, NULL) == NULL){
     return -1;
   }
+  
   // if the thread to detach is exited 
   else if(thread_to_detach->exited == 1){ 
     return -1;
